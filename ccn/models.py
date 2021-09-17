e2bbb3ae5d96ede13746335407cf7c466239cd95
Maxwell <max15borges@gmail.com>
2021-09-17 02:28:11 +0000

    melhorias

--- ccn/models.py
+++ ccn/models.py
@@ -128,102 +128,15 @@ class Postagens(models.Model):
             p.COD_ISSN_L, p.HOME_PAGE,p.SIT_PUBL,p2.DES,pt.TITULO,l.UF_COD,p.PAI_COD
             ORDER BY tit_proprio
         '''
-        # consulta = '''
-        # SELECT 
-        #     p.COD AS codigo , 
-        #         CASE
-		#         WHEN p.FREQ = 'F' THEN 'SEMESTRAL'
-		#         WHEN p.FREQ = 'M' THEN 'MENSAL'
-		#         WHEN p.FREQ = 'Q' THEN 'TRIMESTRAL'
-		#         WHEN p.FREQ = 'B' THEN 'BIMESTRAL'
-		#         WHEN p.FREQ = 'S' THEN 'BIMENSAL'
-		#         WHEN p.FREQ = '?' THEN 'DESCONHECIDO' 
-		#         WHEN  p.FREQ = 'A' THEN 'ANUAL'
-		#         WHEN  p.FREQ = 'T' THEN 'QUADRIMESTRAL'
-		#         WHEN  p.FREQ = 'K' THEN 'IRREGULAR'
-		#         WHEN  p.FREQ = 'Z' THEN 'OUTRAS'
-		#         WHEN  p.FREQ = 'W' THEN 'SEMANAL'
-		#         WHEN  p.FREQ = 'H' THEN 'TRIENAL'
-		#         WHEN  p.FREQ = 'I' THEN 'TRÊS VEZES NA SEMANA'
-		#         WHEN  p.FREQ = 'J' THEN 'TRÊS VEZES NO MES'
-		#         WHEN  p.FREQ = 'D' THEN 'DIARIA'
-		#         WHEN  p.FREQ = 'C' THEN 'BISSEMANAL'
-		#         WHEN  p.FREQ = 'E' THEN 'QUINZENAL'
-		#         WHEN  p.FREQ = 'G' THEN 'BIENAL'
-		#         ELSE p.FREQ 
-        #         END AS frequencia, 
-        #     p.TIT_PROPRIO, 
-        #     p.TITULO_ABREVIADO, 
-        #     p.COD_CCN,
-        #     p.COD_ISSN,
-        #     p.HOME_PAGE,
-        #     CASE
-        #    	    WHEN p.SIT_PUBL='D' THEN 'D'
-        #    	    ELSE 'C'
-        #         END AS situacao, 
-        #     WM_CONCAT(DISTINCT pd.DESIGNACAO || '$$') AS designacoes, 
-        #     CASE l.UF_COD WHEN 'XX' THEN 
-        #     WM_CONCAT(DISTINCT l.DES || ', ' || p.PAI_COD ||': '|| e.NOME || '$$')
-        #     ELSE
-        #     WM_CONCAT(DISTINCT l.DES || ', ' || l.UF_COD ||': '|| e.NOME || '$$')
-        #     END AS imprentas, 
-        #     p2.DES AS pais,
-        #     WM_CONCAT(DISTINCT i.DES) AS idioma,
-        #     WM_CONCAT(DISTINCT s.DES) AS assunto,
-        #     pt.TITULO AS titulo_adicional
-        # FROM ccn.publicacao p
-        #     LEFT JOIN ccn.PUBLICACAO_TITULO pt
-        # 		ON pt.PUBL_COD=p.COD AND pt.TIPO = '09'
-        #     LEFT JOIN ccn.PUBLICACAO_DESIGNACAO pd
-        #         ON p.COD=pd.PUBL_COD
-        #     LEFT JOIN ccn.PUBLICACAO_IMPRENTA pub_i
-        #         ON p.COD = pub_i.PUBL_COD
-        #     LEFT JOIN ccn.EDITORA e
-        #         ON pub_i.EDTO_COD = e.COD
-        #     LEFT JOIN ccn.LOCALIDADE l
-        #         ON pub_i.MUNI_COD = l.COD
-        #     LEFT JOIN ccn.PAIS p2
-        #         ON p.PAI_COD = p2.COD
-        #     LEFT JOIN ccn.PUBLICACAO_IDIOMA pi2
-        #         ON p.COD = pi2.PUBL_COD
-        #     LEFT JOIN ccn.IDIOMA i
-        #         ON pi2.IDIO_COD = i.COD
-        #     LEFT JOIN ccn.PUBLICACAO_SPINES ps
-        #    		ON p.COD=ps.PUBL_COD 
-        #   	LEFT JOIN ccn.SPINES s
-        #    		ON ps.SPIN_COD = s.COD
-        #    WHERE '''
-
-        # for i,k1 in enumerate(valor):
-        #     if(k1):
-        #         if (tipo[i]=='tituloproprio'):
-        #             consulta=consulta+f''' contains(idx_clob,' {k1} within {tipo[i]}')>0 '''
-        #         else:
-        #             consulta=consulta+f''' regexp_like({tipo[i]},'{k1}', 'i') '''
-
-        #         if (juncao[i-1]=='OR'):
-        #             posicao = (30+len(k1)+29+(len(valor[i-1])))*-1
-        #             consulta = consulta[:posicao]+'('+consulta[posicao:]+') '
-                
-        #         if (juncao[i]=='NOT'):
-        #             consulta=consulta+f'''AND'''
-
-        #         if (i<(len(valor)-1)):
-        #             consulta=consulta+f''' {juncao[i]} '''
-        # if (codigo!=''):
-        #     consulta = consulta+f''' AND p.COD in ({codigo}) '''
-
-        # consulta = consulta+f'''
-        # GROUP BY p.COD, p2.DES,p.FREQ,p.TIT_PROPRIO,p.TITULO_ABREVIADO,p.COD_CCN,p.COD_ISSN, p.HOME_PAGE,p.SIT_PUBL, p2.DES,pt.TITULO,l.UF_COD
-        # ORDER BY tit_proprio;'''
         result = consulta.find('%')
         if (result != -1):
             consulta=consulta.replace('%','%%')
-        print (consulta)
         return consulta
 
 class Universidades(models.Model):
     seq = models.IntegerField(primary_key=True)
+    publ_cod = models.CharField(max_length=100)
+    bibl_cod = models.CharField(max_length=100)
     conteudo = models.CharField(max_length=500)
     uf_cod = models.CharField(max_length=100)
     sigla = models.CharField(max_length=100)
@@ -232,10 +145,22 @@ class Universidades(models.Model):
     bairro = models.CharField(max_length=100)
     des = models.CharField(max_length=100)
     cep = models.CharField(max_length=100)
-    publ_cod = models.CharField(max_length=100)
+    numero = models.CharField(max_length=100)
+    tipo = models.CharField(max_length=100)
+
 
     def select (self,codigo):
-        consulta = f'''SELECT pc.SEQ , pc.CONTEUDO, l.UF_COD, b.SIGLA, b.NOME, b.LOGRADOURO, b.BAIRRO, l.DES, b.CEP, PUBL_COD FROM ccn.PUBLICACAO_COLECAO pc, ccn.BIBLIOTECA b, ccn.LOCALIDADE l WHERE PUBL_COD IN ({codigo}) AND b.COD = pc.BIBL_COD AND l.COD = b.MUNI_COD  ORDER BY l.UF_COD, b.SIGLA ;'''
+        consulta = f'''
+        SELECT DISTINCT
+        pc.PUBL_COD, bc.BIBL_COD, pc.SEQ , l.UF_COD, b.SIGLA, b.NOME, b.LOGRADOURO,
+        b.BAIRRO, l.DES, b.CEP, bc.NUMERO, bc.TIPO , pc.CONTEUDO
+        FROM ccn.PUBLICACAO_COLECAO pc
+        INNER JOIN ccn.BIBLIOTECA b ON b.COD = pc.BIBL_COD
+        INNER JOIN ccn.BIBLIOTECA_CONTATO bc ON pc.BIBL_COD = bc.BIBL_COD
+        INNER JOIN ccn.LOCALIDADE l ON l.COD = b.MUNI_COD
+        WHERE
+        pc.PUBL_COD IN ({codigo})
+        ORDER BY l.UF_COD, b.SIGLA,pc.SEQ  ;'''
         return consulta
         
     
--- ccn/views.py
+++ ccn/views.py
@@ -66,6 +66,7 @@ def busca(request):
 def resultado(request):
     lista_itens=''
     lista_postagens=[]
+    lista_universidades=[]
     if request.method == "POST":
         for post in request.POST:
             if (post!='csrfmiddlewaretoken' and post!='formatoConsulta'):
@@ -73,9 +74,30 @@ def resultado(request):
 
         if (request.POST['formatoConsulta']=='Detalhado'):
             lista_universidades = list(Universidades.objects.using('primary').raw(Universidades().select(lista_itens[:-1])))
-        print ('a')
+            _tempNumero = []
+            _tempTipo = []
+            _tempPos =[]
+            for i,universidade in enumerate(lista_universidades):
+                if lista_universidades[i].seq != lista_universidades[i-1].seq and i!=0:
+                    lista_universidades[i-1].numero = _tempNumero
+                    _tempNumero =[]
+                else:
+                    if i!=0:
+                        _tempPos.append(i-1)
+                
+                if (int(lista_universidades[i].tipo) == 1 or  int(lista_universidades[i].tipo) == 2):
+                    _tempNumero.append("TELEFONE: "+lista_universidades[i].numero)
+                if (int(lista_universidades[i].tipo) == 3):
+                    _tempNumero.append("EMAIL: "+lista_universidades[i].numero)
+                if (int(lista_universidades[i].tipo) == 4):
+                    _tempNumero.append("HOME PAGE: "+lista_universidades[i].numero)
+
+                if(i==(len(lista_universidades)-1)):
+                    lista_universidades[i].numero = _tempNumero
+                    for pos in reversed(_tempPos):
+                        lista_universidades.pop(pos)
+
         lista_postagens = list(Postagens.objects.using('primary').raw(Postagens().select(request.session['v1'],request.session['v2'],request.session['v3'],lista_itens[:-1])))
-        
         for i,postagem in enumerate(lista_postagens):
             _temp = []
             for imprenta in reversed((postagem.imprentas).split('$$')[:-1]):
