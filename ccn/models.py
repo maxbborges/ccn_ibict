5c1ca36001ecdd4b8b7591a0e5e6830627dd4680
Maxwell <max15borges@gmail.com>
2022-01-20 13:22:45 +0000

    Melhorias nos models

--- ccn/models.py
+++ ccn/models.py
@@ -1,137 +1,99 @@
 from django.db import models
-import urllib
-
-class Postagens_titulo(models.Model):
-    codigo = models.IntegerField(primary_key=True)
-    titulo_completo = models.CharField(max_length=100)
-    def get_titulo_completo (self,codigo):
-        consulta = f'''SELECT p.COD as codigo, pt.TITULO_COMPLETO FROM ccn.PUBLICACAO p ,ccn.PUBLICACAO_TITULO pt WHERE p.COD = pt.PUBL_COD AND pt.PUBL_COD IN ({codigo}) AND pt.TITULO_COMPLETO IS NOT NULL;'''
-        return consulta
-
 
 class Postagens(models.Model):
-    codigo = models.IntegerField(primary_key=True)
-    frequencia = models.CharField(max_length=100)
-    tit_proprio = models.CharField(max_length=100)
-    titulo_abreviado = models.CharField(max_length=100)
-    cod_ccn = models.CharField(max_length=100)
-    cod_issn = models.CharField(max_length=100)
-    cod_issn_online = models.CharField(max_length=100)
-    home_page = models.CharField(max_length=100)
-    situacao = models.CharField(max_length=100)
-    designacoes = models.CharField(max_length=100)
-    imprentas = models.TextField(max_length=100)
-    pais = models.CharField(max_length=100)
-    idioma = models.CharField(max_length=100)
-    assunto = models.CharField(max_length=100)
-    titulo_adicional = models.CharField(max_length=100)
-    titulo_completo = models.CharField(max_length=100)
+    class Meta:
+        db_table='"ccn"."publicacao"'
 
-    def select (self,tipo,valor,juncao,codigo):
-        consulta = f'''
-            SELECT DISTINCT
-            p.COD as codigo ,
-            CASE
-		        WHEN p.FREQ = 'F' THEN 'SEMESTRAL'
-		        WHEN p.FREQ = 'M' THEN 'MENSAL'
-		        WHEN p.FREQ = 'Q' THEN 'TRIMESTRAL'
-		        WHEN p.FREQ = 'B' THEN 'BIMESTRAL'
-		        WHEN p.FREQ = 'S' THEN 'BIMENSAL'
-		        WHEN p.FREQ = '?' THEN 'DESCONHECIDO' 
-		        WHEN  p.FREQ = 'A' THEN 'ANUAL'
-		        WHEN  p.FREQ = 'T' THEN 'QUADRIMESTRAL'
-		        WHEN  p.FREQ = 'K' THEN 'IRREGULAR'
-		        WHEN  p.FREQ = 'Z' THEN 'OUTRAS'
-		        WHEN  p.FREQ = 'W' THEN 'SEMANAL'
-		        WHEN  p.FREQ = 'H' THEN 'TRIENAL'
-		        WHEN  p.FREQ = 'I' THEN 'TRÊS VEZES NA SEMANA'
-		        WHEN  p.FREQ = 'J' THEN 'TRÊS VEZES NO MES'
-		        WHEN  p.FREQ = 'D' THEN 'DIARIA'
-		        WHEN  p.FREQ = 'C' THEN 'BISSEMANAL'
-		        WHEN  p.FREQ = 'E' THEN 'QUINZENAL'
-		        WHEN  p.FREQ = 'G' THEN 'BIENAL'
-		        ELSE p.FREQ 
-                END AS frequencia, 
-            p.TIT_PROPRIO as ,
-            p.TITULO_ABREVIADO as titulo_abreviado,
-            p.COD_CCN as cod_ccn,
-            p.COD_ISSN as cod_issn,
-            p.COD_ISSN_L as cod_issn_online,
-            p.HOME_PAGE as home_page,
-            CASE WHEN p.SIT_PUBL='D' THEN 'N SEI' 
-            ELSE p.SIT_PUBL END AS situacao,
-            WM_CONCAT(DISTINCT pd.DESIGNACAO || '$$') AS designacoes,
-            CASE l.UF_COD WHEN 'XX' THEN
-            WM_CONCAT(DISTINCT l.DES || ', ' || p.PAI_COD ||': '|| e.NOME || '$$')
-            ELSE (SELECT WM_CONCAT(DISTINCT l.DES || ', ' || l.UF_COD ||': '|| e.NOME || '$$')
-            FROM ccn.publicacao p
-            LEFT JOIN ccn.PUBLICACAO_TITULO pt ON pt.PUBL_COD=p.COD AND pt.TIPO = '01'
-            INNER JOIN ccn.PUBLICACAO_DESIGNACAO pd
-            ON p.COD=pd.PUBL_COD
-            INNER JOIN ccn.PUBLICACAO_IMPRENTA pub_i
-            ON p.COD = pub_i.PUBL_COD
-            INNER JOIN ccn.EDITORA e
-            ON pub_i.EDTO_COD = e.COD
-            INNER JOIN ccn.LOCALIDADE l
-            ON pub_i.MUNI_COD = l.COD
-            WHERE contains(idx_clob,'102250-4 within codccn')>0) END AS imprentas,
-            p2.DES AS pais,
-            WM_CONCAT(DISTINCT i.DES) AS idioma,
-            WM_CONCAT(DISTINCT s.DES) AS assunto,
-            pt.TITULO as titulo_adicional
-            FROM ccn.publicacao p LEFT JOIN ccn.PUBLICACAO_TITULO pt
-            ON pt.PUBL_COD=p.COD AND pt.TIPO = '01'
-            INNER JOIN ccn.PUBLICACAO_DESIGNACAO pd
-            ON p.COD=pd.PUBL_COD
-            INNER JOIN ccn.PUBLICACAO_IMPRENTA pub_i
-            ON p.COD = pub_i.PUBL_COD
-            INNER JOIN ccn.EDITORA e
-            ON pub_i.EDTO_COD = e.COD
-            INNER JOIN ccn.LOCALIDADE l
-            ON pub_i.MUNI_COD = l.COD
-            INNER JOIN ccn.PAIS p2
-            ON p.PAI_COD = p2.COD
-            INNER JOIN ccn.PUBLICACAO_IDIOMA pi2
-            ON p.COD = pi2.PUBL_COD
-            INNER JOIN ccn.IDIOMA i
-            ON pi2.IDIO_COD = i.COD
-            INNER JOIN ccn.PUBLICACAO_SPINES ps
-            ON p.COD=ps.PUBL_COD
-            INNER JOIN ccn.SPINES s
-            ON ps.SPIN_COD = s.COD
-            WHERE '''
+    cod = models.IntegerField(primary_key=True) #cod
+    pais = models.CharField(max_length=100,db_column='pai_cod') #pai_cod
+    _frequencia = models.CharField(max_length=100,db_column='freq') #freq
+    tit_proprio = models.CharField(max_length=100) #tit_proprio
+    titulo_abreviado = models.CharField(max_length=100) #tit_abreviado
+    cod_ccn = models.CharField(max_length=100) #cod_ccn
+    cod_issn = models.CharField(max_length=100) #cod_issn
+    cod_issn_online = models.CharField(max_length=100,db_column='cod_issn_l') #cod_issn_l
+    home_page = models.CharField(max_length=100) #home_page
+    situacao = models.CharField(max_length=100,db_column='sit_publ') #sit_publ
 
-        for i,k1 in enumerate(valor):
-            if(k1):
-                if (tipo[i]=='tituloproprio'):
-                    consulta=consulta+f''' contains(idx_clob,' {k1} within {tipo[i]}')>0 '''
-                elif (tipo[i]=='s.DES'):
-                    consulta=consulta+f''' UPPER({tipo[i]}) LIKE UPPER('{k1}') '''
-                else:
-                    consulta=consulta+f''' regexp_like({tipo[i]},'{k1}', 'i') '''
+    @property
+    def frequencia(self):
+        if self._frequencia == 'Q':
+            return 'TRIMESTRAL'
+        if self._frequencia == 'M':
+            return 'MENSAL'
+        if self._frequencia == 'B':
+            return 'BIMESTRAL'
+        if self._frequencia == 'B':
+            return 'BIMESTRAL'
+        if self._frequencia == 'S':
+            return 'BIMENSAL'
+        if self._frequencia == '?':
+            return 'DESCONHECIDO'
+        if self._frequencia == 'A':
+            return 'ANUAL'
+        if self._frequencia == 'T':
+            return 'QUADRIMESTRAL'
+        if self._frequencia == 'K':
+            return 'IRREGULAR'
+        if self._frequencia == 'Z':
+            return 'OUTRAS'
+        if self._frequencia == 'W':
+            return 'SEMANAL'
+        if self._frequencia == 'H':
+            return 'TRIENAL'
+        if self._frequencia == 'I':
+            return 'TRÊS VEZES NA SEMANA'
+        if self._frequencia == 'J':
+            return 'TRÊS VEZES NO MES'
+        if self._frequencia == 'D':
+            return 'DIARIA'
+        if self._frequencia == 'C':
+            return 'BISSEMANAL'
+        if self._frequencia == 'E':
+            return 'QUINZENAL'
+        if self._frequencia == 'G':
+            return 'BIENAL'
+		
+        return self._frequencia
+    
+    ###### DOCUMENTAÇÃO DA FUNÇÃO SELECT ######
+    # self: os parametros internos
+    # tipo: valor da option do select .slc-chave-tipo (ARRAY COM CADA OPTION PREENCHIDA)
+    # valor: valor do campo input .ipt-valor (ARRAY CADA TEXTO PREENCHIDO)
+    # juncao: valor da option do select .slc-chave-juncao (ARRAY COM CADA OPTION PREENCHIDA)
+    def select (self,tipo,valor,juncao):
+        textoWhere = f''
+        temp=''
+        consulta = f'''select 
+            cod as cod,
+            tit_proprio, 
+            titulo_abreviado, 
+            cod_ccn,
+            cod_issn,
+            cod_issn_l as cod_issn_online,
+            home_page, 
+            sit_publ as situacao, 
+            pai_cod as pais 
+            from ccn.publicacao WHERE '''
+        for i,val in enumerate(valor):
+            if val:
+                textoWhere = textoWhere+f'''contains(idx_clob,'{val} within {tipo[i]}')>0 {juncao[i]} '''
+                temp=juncao[i]
+        textoWhere=textoWhere[:-(len(temp)+1)]
 
-                if (juncao[i-1]=='OR'):
-                    posicao = (30+len(k1)+29+(len(valor[i-1])))*-1
-                    consulta = consulta[:posicao]+'('+consulta[posicao:]+') '
-                
-                if (juncao[i]=='NOT'):
-                    consulta=consulta+f'''AND'''
-
-                if (i<(len(valor)-1)):
-                    consulta=consulta+f''' {juncao[i]} '''
-
-        if (codigo!=''):
-            consulta = consulta+f''' AND p.COD in ({codigo}) '''
-        
-        consulta = consulta+f'''
-            GROUP BY p.COD, p2.DES,p.FREQ,p.TIT_PROPRIO,p.TITULO_ABREVIADO,p.COD_CCN,p.COD_ISSN,
-            p.COD_ISSN_L, p.HOME_PAGE,p.SIT_PUBL,p2.DES,pt.TITULO,l.UF_COD,p.PAI_COD
-            ORDER BY tit_proprio
-        '''
-        result = consulta.find('%')
-        if (result != -1):
-            consulta=consulta.replace('%','%%')
+        consulta = consulta+textoWhere+'ORDER by cod'
+        print (consulta)
         return consulta
+
+class Titulos(models.Model):
+    class Meta:
+        db_table='"ccn"."publicacao_titulo"'    
+
+    publ_cod = models.IntegerField(primary_key=True)
+    tipo = models.CharField(max_length=100,db_column='tipo')
+    titulo = models.CharField(max_length=100,db_column='titulo')
+    seq = models.IntegerField(db_column='seq')
+    titulo_completo = models.TextField(db_column='TITULO_COMPLETO')
 
 class Universidades(models.Model):
     seq = models.IntegerField(primary_key=True)
@@ -148,7 +110,6 @@ class Universidades(models.Model):
     numero = models.CharField(max_length=100)
     tipo = models.CharField(max_length=100)
 
-
     def select (self,codigo):
         consulta = f'''
         SELECT DISTINCT
@@ -162,7 +123,53 @@ class Universidades(models.Model):
         pc.PUBL_COD IN ({codigo})
         ORDER BY l.UF_COD, b.SIGLA,pc.SEQ  ;'''
         return consulta
-        
+
+class Impretas(models.Model):
+    class Meta:
+        db_table='"ccn"."publicacao_imprenta"'
+
+    publ_cod=models.IntegerField(primary_key=True)
+    edto_cod=models.CharField(max_length=100,db_column='edto_cod')
+    muni_cod=models.CharField(max_length=100,db_column='muni_cod')
+
+class Editoras(models.Model):
+    class Meta:
+        db_table='"ccn"."editora"'
+
+    cod=models.CharField(primary_key=True,max_length=100)
+    nome=models.CharField(max_length=100,db_column='nome')
+
+class Localidades(models.Model):
+    class Meta:
+        db_table='"ccn"."localidade"'
+
+    cod=models.IntegerField(primary_key=True)
+    des=models.CharField(max_length=100,db_column='des')
+    pai_cod=models.CharField(max_length=100,db_column='pai_cod')
+
+class Spine(models.Model):
+    class Meta:
+        db_table='"ccn"."publicacao_spines"'
+
+    publ_cod=models.IntegerField(primary_key=True)
+    spin_cod=models.IntegerField(db_column='spin_cod')
+
+class Assuntos(models.Model):
+    class Meta:
+        db_table='"ccn"."spines"'
+
+    cod=models.IntegerField(primary_key=True)
+    des=models.CharField(max_length=100,db_column='des')
+
+class Designacao(models.Model):
+    class Meta:
+        db_table='"ccn"."PUBLICACAO_DESIGNACAO"'
+
+    publ_cod=models.IntegerField(primary_key=True)
+    designacao=models.CharField(max_length=100,db_column='designacao')
+
+
+
     
 
 
--- ccn/views.py
+++ ccn/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect,get_list_or_404, get_object_or_404
 from django.core.paginator import Paginator
 from django.http import HttpResponse
 from django.core.paginator import Paginator
-from .models import Postagens,Universidades,Postagens_titulo
+from .models import Postagens,Universidades,Titulos,Impretas,Editoras,Localidades,Spine,Assuntos,Designacao
 from django.utils import timezone
 from django.urls import reverse
 from django.db import connection
@@ -12,15 +12,15 @@ from django.contrib.sessions.backends.db import SessionStore
 def index(request):
     camposConsulta ={
        'Título':'tituloproprio',
-       'Assunto':'s.DES',
-       'País de publicação':'p2.des',
-       'Idioma do texto':'i.des',
-       'Código CCN':'COD_CCN',
-       'Número ISSN':'COD_ISSN',
-       'Situação de publicação':'SIT_PUBL',
-       'Local de edição/publicação':'l.des',
-       'Editor/Publicador':'e.nome',
-       'Título abreviado':'TITULO_ABREVIADO'
+       'Assunto':'assunto',
+       'País de publicação':'pais',
+       'Idioma do texto':'idioma',
+       'Código CCN':'codccn',
+       'Número ISSN':'codissn',
+       'Situação de publicação':'sitpubl',
+       'Local de edição/publicação':'imprenta',
+       'Editor/Publicador':'local',
+       'Título abreviado':'tituloabreviado'
     }
     return render(request,'index.html',{'campos':camposConsulta})
 
@@ -33,47 +33,190 @@ def busca(request):
         '50':'50',
         '60':'60'
     }
+    
     if(request.GET.get('qtdItens')):
         request.session['qtdItens'] =request.GET.get('qtdItens') 
-    if request.method == "GET":
-        post_list= Postagens().select(request.session['v1'],request.session['v2'],request.session['v3'],'')
-        
-        lista = Postagens.objects.using('primary').raw(post_list)
-        page_number = request.GET.get('page')
-        paginator = Paginator(lista,request.session['qtdItens'])
-        page_obj = paginator.page(page_number)
 
     if request.method == "POST":
         request.session['v1'] = (request.POST).getlist('tipo')
         request.session['v2'] = (request.POST).getlist('valor')
         request.session['v3'] = (request.POST).getlist('juncao')
         request.session['qtdItens'] = 10
-        post_list= Postagens().select(request.session['v1'],request.session['v2'],request.session['v3'],'')
-        lista = Postagens.objects.using('primary').raw(post_list)
+    
+    # RECUPERA AS POSTAGENS DO MODEL POSTAGEM
+    post_list= Postagens().select(request.session['v1'],request.session['v2'],request.session['v3'])
+    lista = Postagens.objects.using('primary').raw(post_list)
+    
+    if (len(lista)<=0):
+        return render(request,'busca.html',{'postagens': [],'titulos':[],'itens':numeroDeItens,'qtdPostagensBanco':0,'qtdPostagensPagina':0})
+    # RECUPERA OS TITULOS DO MODEL TITULOS
+    listaCodPublicacoes=[]
+    for l in lista:
+        	listaCodPublicacoes.append(l.cod)
 
-        page_number = request.GET.get('page',1)
-        paginator = Paginator(lista,request.session['qtdItens'])
-        page_obj = paginator.page(page_number)
+    tituloslistaResult = Titulos.objects.using('primary').filter(publ_cod__in=listaCodPublicacoes)
+
+    # UNE OS TITULOS OBTIDOS DO MODEL
+    oldCod=0
+    arrayTitulos=[]
+    for i,titulos in enumerate(tituloslistaResult):
+        cod=titulos.publ_cod
+        if oldCod==0:
+            oldCod=cod
+            titulo=''
+        if cod!=oldCod:
+            arrayTitulos.append(titulo)
+            oldCod=cod
+            titulo=''
+        if (titulos.tipo=='01'):
+            titulo=titulo+titulos.titulo
+        if (titulos.tipo=='02'):
+            titulo=titulo+' ('+titulos.titulo+')'
+        if (titulos.tipo=='07'):
+            titulo=titulo+' : '+titulos.titulo
+        if (titulos.tipo=='08'):
+            titulo=titulo+' . '+titulos.titulo
+        if (titulos.tipo=='11'):
+            titulo=titulo+' = '+titulos.titulo
+        if (titulos.tipo=='14'):
+            titulo=titulo+' / '+titulos.titulo
+        if (titulos.tipo=='15'):
+            titulo=titulo+' . '+titulos.titulo
+
+        if i==len(tituloslistaResult)-1:
+            arrayTitulos.append(titulo)
+        
+    paginator = Paginator(lista,request.session['qtdItens'])
+    page = request.GET.get('page')
+    pageLista = paginator.get_page(page)
 
     qtdPostagensPagina = int(request.session['qtdItens'])
     qtdPostagensBanco = len(list(lista))
+
+    if (page=='1' or not page):
+        arrayTitulos = arrayTitulos[0:qtdPostagensPagina-1]
+    else:
+        arrayTitulos = arrayTitulos[qtdPostagensPagina*(int(page)-1):(qtdPostagensPagina*int(page))-1]
+
     if (qtdPostagensPagina>qtdPostagensBanco):
         qtdPostagensPagina = qtdPostagensBanco
         
-    return render(request,'busca.html',{'postagens': page_obj,'itens':numeroDeItens,'qtdPostagensBanco':qtdPostagensBanco,'qtdPostagensPagina':qtdPostagensPagina})
+    return render(request,'busca.html',{'postagens': pageLista,'titulos':arrayTitulos,'itens':numeroDeItens,'qtdPostagensBanco':qtdPostagensBanco,'qtdPostagensPagina':qtdPostagensPagina})
     
 
 def resultado(request):
-    lista_itens=''
+    lista_itens2=[]
     lista_postagens=[]
     lista_universidades=[]
+    arrayTitulos=[]
+    arrayAssuntos=[]
+    arrayDesignacoes=[]
+    arrayImprenta = []
     if request.method == "POST":
         for post in request.POST:
             if (post!='csrfmiddlewaretoken' and post!='formatoConsulta'):
-                lista_itens=lista_itens+"'"+str(post)+"'"+","
+                lista_itens2.append(str(post))
+        
+        # Recupera todas as publicações selecionadas
+        lista_postagens = list(Postagens.objects.using('primary').filter(cod__in=lista_itens2))
+
+        # Recupera todos os titulo das publicações
+        lista_titulos = Titulos.objects.using('primary').filter(publ_cod__in=lista_itens2)
+
+        lista_designacoes = Designacao.objects.using('primary').filter(publ_cod__in=lista_itens2)
+        texto=''
+        for i,designacao in enumerate(lista_designacoes):
+            texto = texto + designacao.designacao + '\n'
+            if (i!=len(lista_designacoes)-1):
+                if (lista_designacoes[i].publ_cod==lista_designacoes[i+1].publ_cod):
+                    continue
+            
+            arrayDesignacoes.append(texto)
+            texto=''
+
+        # Recupera todas as imprentas das publicações
+        lista_imprentas = Impretas.objects.using('primary').filter(publ_cod__in=lista_itens2)
+        oldCod=0
+        
+        editoras=[]
+        localidades=[]
+        for i,imprentas in enumerate(lista_imprentas):
+            editoras.append(imprentas.edto_cod)
+            localidades.append(imprentas.muni_cod)
+
+            # Verifica se é a ultima posição do array
+            if (i!=len(lista_imprentas)-1):
+                if (lista_imprentas[i].publ_cod==lista_imprentas[i+1].publ_cod):
+                    continue
+
+            editora = Editoras.objects.using('primary').filter(cod__in=editoras).order_by('nome')
+
+            texto=''
+            for i,e in enumerate(editora):
+                localidade = list(Localidades.objects.using('primary').filter(cod=localidades[i]))
+                texto=texto+localidade[0].des+', '
+                texto=texto+localidade[0].pai_cod+': '
+                texto=texto+e.nome+'\n'
+                
+            arrayImprenta.append(texto)
+
+            editoras=[]
+            localidades=[]
+
+
+        # Recupera todos os assuntos das publicações
+        lista_spine=Spine.objects.using('primary').filter(publ_cod__in=lista_itens2)
+        oldCod=0
+        for i,spine in enumerate(lista_spine):
+            cod=spine.publ_cod
+            if oldCod==0:
+                textoSpine=''
+                oldCod=spine.publ_cod
+
+            if cod!=oldCod:
+                arrayAssuntos.append(textoSpine[:-2])
+                oldCod=cod
+                textoSpine=''
+            
+            lista_assuntos=Assuntos.objects.using('primary').filter(cod=spine.spin_cod)
+            for assunto in lista_assuntos:
+                textoSpine=textoSpine+assunto.des+', '
+            
+            if i==len(lista_spine)-1:
+                arrayAssuntos.append(textoSpine[:-2])
+
+        # Formata os titulos para o padrão de exibição
+        oldCod=0
+        for i,titulos in enumerate(lista_titulos):
+            cod=titulos.publ_cod
+            if oldCod==0:
+                oldCod=cod
+                titulo=''
+            if cod!=oldCod:
+                arrayTitulos.append(titulo)
+                oldCod=cod
+                titulo=''
+            if (titulos.tipo=='01'):
+                titulo=titulo+titulos.titulo
+            if (titulos.tipo=='02'):
+                titulo=titulo+' ('+titulos.titulo+')'
+            if (titulos.tipo=='07'):
+                titulo=titulo+' : '+titulos.titulo
+            if (titulos.tipo=='08'):
+                titulo=titulo+' . '+titulos.titulo
+            if (titulos.tipo=='11'):
+                titulo=titulo+' = '+titulos.titulo
+            if (titulos.tipo=='14'):
+                titulo=titulo+' / '+titulos.titulo
+            if (titulos.tipo=='15'):
+                titulo=titulo+' . '+titulos.titulo
+
+            if i==len(lista_titulos)-1:
+                arrayTitulos.append(titulo)
+        
 
         if (request.POST['formatoConsulta']=='Detalhado'):
-            lista_universidades = list(Universidades.objects.using('primary').raw(Universidades().select(lista_itens[:-1])))
+            lista_universidades = list(Universidades.objects.using('primary').raw(Universidades().select(','.join(lista_itens2))))
             _tempNumero = []
             _tempTipo = []
             _tempPos =[]
@@ -97,16 +240,11 @@ def resultado(request):
                     for pos in reversed(_tempPos):
                         lista_universidades.pop(pos)
 
-        lista_postagens = list(Postagens.objects.using('primary').raw(Postagens().select(request.session['v1'],request.session['v2'],request.session['v3'],lista_itens[:-1])))
-        for i,postagem in enumerate(lista_postagens):
-            _temp = []
-            for imprenta in reversed((postagem.imprentas).split('$$')[:-1]):
-                if (imprenta[0]==','):
-                    imprenta=imprenta[1:]
-                
-                _temp.append(imprenta)
-            lista_postagens[i].imprentas = _temp
-
-        lista_titulos = Postagens_titulo.objects.using('primary').raw(Postagens_titulo().get_titulo_completo(lista_itens[:-1]))
-
-    return render(request, 'resultado.html',{'lista_postagens': lista_postagens,'lista_universidades':lista_universidades,'lista_titulos':lista_titulos})
+    return render(request, 'resultado.html',{
+        'lista_postagens': lista_postagens,
+        'lista_universidades':lista_universidades,
+        'lista_titulos':arrayTitulos,
+        'imprentas':arrayImprenta,
+        'assuntos':arrayAssuntos,
+        'designacoes':arrayDesignacoes,
+        'formatoConsulta':request.POST['formatoConsulta']})
