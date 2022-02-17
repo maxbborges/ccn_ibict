e9a7d3df61565cdcb614e376c31edb42e5e9196b
Maxwell <max15borges@gmail.com>
2022-02-17 21:27:38 +0000

    Inserido Hover

--- ccn/static/js/relacao.js
+++ ccn/static/js/relacao.js
@@ -1,3 +1,14 @@
 $('.btnRetornar').click(function(){
     window.location.replace("/")
 })
+
+$( document ).ready(function() {
+    $( ".universidade_hover" ).hover(function() {
+        var info = $($(window.event.target).parents()[1]).find('#info_adicionais')
+        if ($(info).css("display")=='block'){
+            $(info).css("display","none")
+        } else {
+            $(info).css("display","block")
+        }
+    });
+});
